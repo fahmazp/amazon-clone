18b8cf89dd03039eaee3bd00eae50293c2c26e5d
fahmazp <fahmazashraf7@gmail.com>
2025-10-23 11:45:43 +0530

    index file changed

--- server/index.js
+++ server/index.js
@@ -48,5 +48,6 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
